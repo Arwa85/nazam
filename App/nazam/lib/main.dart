b4d9bafeef70c798cff2f9132b85d986c5ae0ea2
arwa85 <arwaalafifi85@gmail.com>
2023-09-27 19:11:42 +0300

    fix main

--- App/nazam/lib/main.dart
+++ App/nazam/lib/main.dart
@@ -1,27 +1,42 @@
-
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
- 
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(useMaterial3: true),
-      home: const Nazemm(),
-    );
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage());
   }
 }
 
-class Nazemm extends StatelessWidget {
-const Nazemm({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      color: Colors.black,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Nazam'),
+          centerTitle: true,
+          backgroundColor: const Color.fromRGBO(217, 217, 217, 1),
+        ),
+      ),
+    );
   }
 }
