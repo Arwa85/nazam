24c01d6dad22bdb7e93f4422c27d4448a6b09f01
arwa85 <arwaalafifi85@gmail.com>
2023-09-28 16:17:52 +0300

    delet home page from main

--- App/nazam/lib/main.dart
+++ App/nazam/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nazam/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,75 +16,6 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      color: Colors.black,
-      home: SafeArea(
-        child: DefaultTabController(
-          length: 3,
-          child: Scaffold(
-            body: Stack(children: <Widget>[
-              TabBarView(
-                children: [
-                  Container(
-                    color: Colors.white,
-                  ),
-                  Container(
-                    color: Colors.black,
-                  ),
-                  Container(
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-              Container(
-                height: 20,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30),
-                    bottomRight: Radius.circular(30),
-                  ),
-                  color: Color.fromRGBO(238, 235, 235, 1),
-                ),
-              )
-            ]),
-            appBar: AppBar(
-                elevation: 0,
-                backgroundColor: const Color.fromRGBO(238, 235, 235, 1),
-                title: const TabBar(
-                  tabs: [
-                    Tab(
-                      icon: Icon(Icons.account_circle),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.home),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.list),
-                    ),
-                  ],
-                  labelColor: Color.fromRGBO(43, 101, 109, 1),
-                  unselectedLabelColor: Colors.black,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  indicatorPadding: EdgeInsets.all(8.0),
-                )),
-            backgroundColor: const Color.fromRGBO(238, 235, 235, 1),
-          ),
-        ),
-      ),
-    );
   }
 }
 
@@ -105,11 +37,11 @@ class WelcomePage extends StatelessWidget {
             Text(
               "مرحبا بك في نظّم",
               style: TextStyle(
-                  fontSize: 40,
-                  fontFamily: 'Marhey',
-                  height: 1,
-                  color:Color.fromARGB(255, 146, 88, 0),
-            ),
+                fontSize: 40,
+                fontFamily: 'Marhey',
+                height: 1,
+                color: Color.fromARGB(255, 146, 88, 0),
+              ),
             ),
           ],
         ),
