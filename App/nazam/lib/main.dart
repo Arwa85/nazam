7916aaefbe884c5a34bebd28c7e7a91691d61722
kholud22 <khuloodandargiri@gmail.com>
2023-09-28 18:48:54 +0300

    log in modfication

--- App/nazam/lib/main.dart
+++ App/nazam/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:nazam/home.dart';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -20,18 +24,136 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
 
 
+class _LoginState extends State<Login> {
+  Map userData = {};
+  final _formkey = GlobalKey<FormState>();
 
-class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login Page'),
+        title: Text('Login'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text('Login Page Content'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Center(
+                child: Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    border: Border.all(color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                  child: Image.asset('images/logo.png'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: TextFormField(
+                          validator: MultiValidator([
+                            RequiredValidator(errorText: 'Enter email address'),
+                            EmailValidator(errorText: 'Please enter a valid email'),
+                          ]),
+                          decoration: InputDecoration(
+                            hintText: 'ادخل البريد الإلكتروني',
+                            labelText: 'البريد الإكتروني',
+                            prefixIcon: Icon(
+                              Icons.email,
+                              //color: Colors.green,
+                            ),
+                            errorStyle: TextStyle(fontSize: 18.0),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red),
+                              borderRadius: BorderRadius.all(Radius.circular(9.0)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: TextFormField(
+                          validator: MultiValidator([
+                            RequiredValidator(errorText: 'Please enter Password'),
+                            MinLengthValidator(8, errorText: 'Password must be at least 8 characters'),
+                            PatternValidator(r'(?=.*?[#!@$%^&*-])', errorText: 'Password must contain at least one special character'),
+                          ]),
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            labelText: 'Password',
+                            prefixIcon: Icon(
+                              Icons.key,
+                              color: Colors.green,
+                            ),
+                            errorStyle: TextStyle(fontSize: 18.0),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red),
+                              borderRadius: BorderRadius.all(Radius.circular(9.0)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(180, 0, 0, 0),
+                        child: Text('Forget Password!'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(28.0),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formkey.currentState!.validate()) {
+                                print('form submitted');
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              primary: Colors.blue,
+                            ),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                     
+                      
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -52,7 +174,7 @@ class _SplashState extends State<Splash> {
     Future.delayed(const Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => LoginPage()),
+        MaterialPageRoute(builder: (context) => Login()),
       );
     });
   }
