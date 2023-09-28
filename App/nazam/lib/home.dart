9677be871e28a3046269c53e6c66d3b26329710a
arwa85 <arwaalafifi85@gmail.com>
2023-09-28 16:20:40 +0300

    delet MyApp class from home page

--- App/nazam/lib/home.dart
+++ App/nazam/lib/home.dart
@@ -1,23 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(
-          primarySwatch: Colors.grey,
-        ),
-        home: const MyHomePage());
-  }
-}
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
