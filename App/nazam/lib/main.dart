ce1fa27c212259cdd0bca51b78421c010c20964f
arwa85 <arwaalafifi85@gmail.com>
2023-09-28 18:07:06 +0300

    delete package

--- App/nazam/lib/main.dart
+++ App/nazam/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nazam/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.grey,
         ),
-        home: Splash());
+        home: const Splash());
   }
 }
 
