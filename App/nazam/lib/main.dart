fa387a34589702ac0104b3d94ca46bc23abda794
kholud22 <khuloodandargiri@gmail.com>
2023-09-29 23:51:08 +0300

    log in

--- App/nazam/lib/main.dart
+++ App/nazam/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Splash(),
+      home: const  Splash(),
     );
   }
 }
