64825b4dc48d379d9722558fbb8ab799795ab58b
arwa85 <arwaalafifi85@gmail.com>
2023-10-03 00:41:41 +0300

    Firebase Configuration

--- App/nazam/lib/main.dart
+++ App/nazam/lib/main.dart
@@ -4,12 +4,11 @@ import 'package:nazam/pages/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 void main() async {
-   WidgetsFlutterBinding.ensureInitialized();
+  //* initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  //Initialize Firebase:
-
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const  Splash(),
+      home: const Splash(),
     );
   }
 }
